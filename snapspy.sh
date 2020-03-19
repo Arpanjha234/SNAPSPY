adc2240458502468b7d18ef3cc2d0c675ccc6f4c
Arpanjha234 <62328473+Arpanjha234@users.noreply.github.com>
2020-03-19 13:48:01 +0530

    Update snapspy.sh

--- snapspy.sh
+++ snapspy.sh
@@ -216,7 +216,7 @@ sed 's+forwarding_link+'$send_link'+g' template.php > index.php
 start() {
 
 default_choose_sub="Y"
-default_subdomain="wishfish$RANDOM"
+default_subdomain="snapspy$RANDOM"
 
 printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Choose subdomain? (Default:\e[0m\e[1;77m [Y/n] \e[0m\e[1;33m): \e[0m'
 read choose_sub
