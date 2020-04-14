bc91a874a98bd887ca6d41c0a4917f85aba2364f
Arpanjha234 <62328473+Arpanjha234@users.noreply.github.com>
2020-04-14 17:47:44 +0530

    Update snapspy.sh

--- snapspy.sh
+++ snapspy.sh
@@ -4,7 +4,8 @@
 trap 'printf "\n";stop' 2
 
 banner() {
-
+printf "\n"
+printf "\n"
 printf " \e[1;92m  ____  _   _    _    ____  ____  ______   __  \e[0m\n"
 printf " \e[1;92m / ___|| \ | |  / \  |  _ \/ ___||  _ \ \ / /  \e[0m\n"
 printf " \e[1;92m \___ \|  \| | / _ \ | |_) \___ \| |_) \ V /   \e[0m\n"
